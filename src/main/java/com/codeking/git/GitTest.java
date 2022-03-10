f3d4d9fdc6aac0e9675823524edf26ef4f1cbe6d
sunshine <1094234526@qq.com>
2022-03-10 19:51:00 +0800

    first commit

---
+++ src/main/java/com/codeking/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.codeking.git;
+
+/**
+ * @author king_xiong
+ * @date 2022-03-10 19:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
